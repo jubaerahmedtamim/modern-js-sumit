deca2c76c67f046942e173b7f251b910d24775c9
Jubaer Ahmed Tamim <jubaerahmedtamim2014@gmail.com>
2025-02-11 23:49:13 +0600

    arrow funtion

---
+++ arrow.js
@@ -0,0 +1,14 @@
+// normal function
+function number1(n){
+    return n;
+}
+console.log(number1(10));
+// simplest syntax of arrow function 
+const number = n => n;
+const numbers = (a, b) => a + b;
+const numbers2 = (a, b) => {
+    return a + b;
+}
+console.log(number(10))
+console.log(numbers(10, 20))
+console.log(numbers2(20, 30))
