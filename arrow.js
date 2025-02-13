f7299d4a958ec9b02fe9ae07bb0a1e957b703378
Jubaer Ahmed Tamim <jubaerahmedtamim2014@gmail.com>
2025-02-13 13:10:13 +0600

    truthy and falsy value

--- arrow.js
+++ arrow.js
@@ -12,3 +12,15 @@ const numbers2 = (a, b) => {
 console.log(number(10))
 console.log(numbers(10, 20))
 console.log(numbers2(20, 30))
+
+var javascript = {
+    name: 'JavaScript',
+    libraries : ["Angular","React", "Vue"],
+    printLibraries: function(){
+        this.libraries.forEach((a)=>{
+            console.log(`${this.name} loves ${a}`);
+        })
+    }
+}
+
+javascript.printLibraries();
---
+++ truthy_falsy.js
@@ -0,0 +1,18 @@
+const mayVar = NaN;
+
+if(mayVar){
+    console.log("I am truthy")
+}
+else{
+    console.log("I am falsy.")
+}
+
+// some falsy value
+/*
+    1. empty string "", but not " " this.
+    2. null
+    3. 0
+    4. undefined
+    5. Nan
+    6. boolean value : false 
+*/
