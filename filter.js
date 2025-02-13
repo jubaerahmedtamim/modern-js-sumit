5f6d726d46fa18f16c9fd5f05eed1d7338b60c4d
Jubaer Ahmed Tamim <jubaerahmedtamim2014@gmail.com>
2025-02-14 01:49:00 +0600

    filter method in js

---
+++ filter.js
@@ -0,0 +1,11 @@
+/*
+filter method returns an array of condition fullfilled elements.
+*/
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const result = numbers.filter(function(currentValue, currentIndex, arr){
+    return currentValue > 8;
+})
+
+console.log(result)
