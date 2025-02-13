ca16834b52f6eb16afbb58d3910e33e8f65ea4b7
Jubaer Ahmed Tamim <jubaerahmedtamim2014@gmail.com>
2025-02-14 02:01:19 +0600

    slice works on array and string

--- filter.js
+++ filter.js
@@ -1,5 +1,6 @@
 /*
 filter method returns an array of condition fullfilled elements.
+This method does not change main array.
 */
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
---
+++ slice.js
@@ -0,0 +1,11 @@
+/*
+    slice method works on array as well as strings.
+*/
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log(numbers.slice(0,3))
+console.log(numbers.slice(-5, 6))
+
+// example of slice on strings.
+console.log("Junaina".slice(0, 5))
