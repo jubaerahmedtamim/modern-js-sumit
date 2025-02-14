2a84c71c5bbadf645839cb8519b5cd48c9b36229
Jubaer Ahmed Tamim <jubaerahmedtamim2014@gmail.com>
2025-02-14 14:13:59 +0600

    array.puhs() in modern js

---
+++ push.js
@@ -0,0 +1,10 @@
+// array.prototype.push()
+// This method changes main or original array.
+
+const number = [1, 2, 3]
+
+console.log("Before push an element to an array:", number)
+
+number.push(4)
+
+console.log("Aftet push an element to an array: ",number)
