5991898a7f96b48a78f28fd4a7ba46ca256119c4
Jubaer Ahmed Tamim <jubaerahmedtamim2014@gmail.com>
2025-02-16 13:02:44 +0600

    Modern objects methods to extract keys and values

---
+++ object.js
@@ -0,0 +1,13 @@
+const myObj = {
+    name: "Javascript",
+    estd: '1995',
+    founder: "Brendan Eich",
+    ranking: 1
+}
+
+const keysArray = Object.keys(myObj)
+const valuesArray = Object.values(myObj) 
+const entriesArray = Object.entries(myObj);
+console.log(keysArray)
+console.log(valuesArray)
+console.log(entriesArray)
